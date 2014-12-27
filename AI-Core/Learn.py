e1ec19d7b19f612badf7b17b2830f3829e189db1
gabrieloli <gabriel.oli01001@gmail.com>
2014-12-27 11:10:15 -0200

    Create Learn.py

---
+++ AI-Core/Learn.py
@@ -0,0 +1,44 @@
+from __future__ import print_function
+import time
+
+print ("1>Learn")
+print ("2>Research")
+
+comma = raw_input(">>>")
+
+if comma == '1':
+	brain = open('brain.txt', 'a')
+	enter = raw_input("Input:")
+	enter_to = raw_input("to:")
+	blank = ' '
+	to_write_on_file = enter + blank + enter_to
+	
+	to_write_on_file_inverse = enter_to + blank + enter
+	
+	print(to_write_on_file, file=brain)
+	print(to_write_on_file_inverse, file=brain)
+	brain.close()
+else:
+	out = open('brain.txt', 'r')
+	print (out.readlines())
+	out.close()
+
+	enter = raw_input("Term:")
+	enter_second = raw_input("and:")
+	full_enter = enter +' '+ enter_second
+
+	if enter_second == '':
+		string = True
+	else:
+		string = False
+
+	if string == True:
+		if enter in open('brain.txt').read():
+			print("true")
+		else:
+			print("false")
+	else:
+		if full_enter in open('brain.txt').read():
+			print("true")
+		else:
+			print("false")
